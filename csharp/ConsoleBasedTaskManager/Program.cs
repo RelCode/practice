da113456f4b0f4c869ebab999f7f6656fd7d1477
RelCode <princefana7@gmail.com>
2025-01-29 14:59:54 +0200

    console based task manager C# app intial commit

---
+++ csharp/ConsoleBasedTaskManager/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleBasedTaskManager
+{
+    internal class Program
+    {
+        static List<Task> tasks = new List<Task>();
+        static void Main(string[] args)
+        {
+            bool exitApp = false;
+            do
+            {
+                AppMenu();
+                Console.ReadLine();
+            } while (!exitApp);
+        }
+
+        static void AppMenu()
+        {
+            Console.WriteLine("============================= MAIN MENU =================================");
+            Console.WriteLine("1. Add Task");
+            Console.WriteLine("2. List Tasks");
+            Console.WriteLine("3. Exit");
+        }
+    }
+}
