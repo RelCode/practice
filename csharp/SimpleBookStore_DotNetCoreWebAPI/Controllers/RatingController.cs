1a7ac875784f7d989f1feda03270af11c7bd181b
RelCode <princefana7@gmail.com>
2025-01-31 21:59:20 +0200

    added update and delete functionality on ratings

--- csharp/SimpleBookStore_DotNetCoreWebAPI/Controllers/RatingController.cs
+++ csharp/SimpleBookStore_DotNetCoreWebAPI/Controllers/RatingController.cs
@@ -27,5 +27,30 @@ namespace SimpleBookStore_DotNetCoreWebAPI.Controllers
             await _context.SaveChangesAsync();
             return new OkResult();
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateRating(int id, Rating updatedRating)
+        {
+            if (id != updatedRating.Id)
+            {
+                return new BadRequestResult();
+            }
+            _context.Entry(updatedRating).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRating(int id)
+        {
+            var rating = await _context.Ratings.FindAsync(id);
+            if (rating == null)
+            {
+                return new NotFoundResult();
+            }
+            _context.Ratings.Remove(rating);
+            await _context.SaveChangesAsync();
+            return new OkResult();
+        }
     }
 }
