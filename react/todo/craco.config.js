69d2adda0a016d8b88754fc021b15481e3a22240
Relebohile Nkosi <32488818+RelCode@users.noreply.github.com>
2025-02-03 07:08:45 +0200

    Update craco.config.js

--- react/todo/craco.config.js
+++ react/todo/craco.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     webpack: {
       configure: (webpackConfig) => {
-        // Example: Adding custom rules or modifying the config
         return webpackConfig;
       },
     },
