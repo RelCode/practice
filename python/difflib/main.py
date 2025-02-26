28afd00145a66c7182e9154eaa5cecf2a2c926ba
RelCode <princefana7@gmail.com>
2025-02-26 20:36:38 +0200

    added .txt file comparison for our diff compare application that uses difflib library, also added changed line indicator as well as highlighting the number of words that were added/removed

--- python/difflib/main.py
+++ python/difflib/main.py
@@ -1,5 +1,6 @@
 import csv
 import difflib
+from pathlib import Path
 from typing import List, Dict
 
 def load_data(file: str) -> Dict[list[str], List[str]]:
@@ -23,14 +24,22 @@ def compare_responses(responses: List[str]) -> None:
             diff = list(d.compare(responses[i].splitlines(), 
                                 responses[j].splitlines()))
             
+            line_num1 = line_num2 = 0
+            
             # Print differences
             for line in diff:
                 if line.startswith('  '): # unchanged
                     print(line)
+                    line_num1 += 1
+                    line_num2 += 1
                 elif line.startswith('- '): # removed
-                    print(f'\033[91m{line}\033[0m')  # red
+                    word_count = len(line[2:].split())
+                    print(f'\033[91m- [L{line_num1+1}]({word_count} words) {line[2:]}\033[0m')  # red
+                    line_num1 += 1
                 elif line.startswith('+ '): # added
-                    print(f'\033[92m{line}\033[0m')  # green
+                    word_count = len(line[2:].split())
+                    print(f'\033[92m+ [L{line_num2+1}]({word_count} words) {line[2:]}\033[0m')  # green
+                    line_num2 += 1
             
             # Calculate similarity
             similarity = difflib.SequenceMatcher(None, 
@@ -38,13 +47,19 @@ def compare_responses(responses: List[str]) -> None:
                                                responses[j]).ratio()
             print(f"\nSimilarity ratio: {similarity:.2%}")
 
-def main():
+def testing_csv():
     filePath = "data/fictional.csv"
     [queries, responses] = load_data(filePath)
     
     for i in range(len(queries)):
         print("=" * 50)
         compare_responses(responses)
+        
+def testing_txt():
+    txt1 = Path("data/doc1.txt").read_text(encoding='utf-8-sig')
+    txt2 = Path("data/doc2.txt").read_text(encoding='utf-8-sig')
+    compare_responses([txt1, txt2])
 
 if __name__ == "__main__":
-    main()
+    # testing_csv()
+    testing_txt()
